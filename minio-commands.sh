cb504cf9382d2b311bc0c5dbda67d4f064e4a2d3
ccat <ccathlete01@gmail.com>
2025-03-12 08:06:42 +0100

    Added documentation for creating a webhook and configuring a minio event using curl.

--- minio-commands.sh
+++ minio-commands.sh
@@ -1,14 +1,14 @@
 !# /bin/bash
 
-. .env
+source .env
 
 # Create a webhook
-curl -X POST "http://<MINIO_HOST>:9000/minio/admin/v3/add/webhook" \
+curl -X POST "http://$MINIO_HOST:$MINIO_API_PORT/minio/admin/v3/add/webhook" \
   -H "Content-Type: application/json" \
   --user "$MINIO_ACCESS_KEY:$MINIO_SECRET_KEY" \
   -d '{
     "config": {
-      "endpoint": "http://<YOUR_APP_HOST>:<YOUR_PORT>/etl/webhook",
+      "endpoint": "http://127.0.0.1:8080/etl/webhook",
       "queueDir": "/tmp/minio-events",
       "queueLimit": 100000
     },
